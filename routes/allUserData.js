1063b0905ca865f7bc50636143cbb5dfee859c10
Rajganez <ganezsouth@live.com>
2024-07-09 19:00:10 +0530

    Small error corrected in the projection of findOne

--- routes/allUserData.js
+++ routes/allUserData.js
@@ -8,7 +8,7 @@ allDataRouter.get("/:UserId", async (req, res) => {
   try {
     const data = await registerCollections.findOne(
       { UserID: id },
-      { projection: { _id } }
+      { projection: { _id : 0 } }
     );
     res.status(200).send({ msg: "Retreived Successfully", data });
   } catch (error) {
