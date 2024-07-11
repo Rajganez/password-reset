754dec6d70471ed0864a713db332cae89d72bc3e
Rajganez <ganezsouth@live.com>
2024-07-11 13:58:14 +0530

    few changes done

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -24,35 +24,37 @@ resetRouter.post("/:idforParam", async (req, res) => {
       return res.status(404).json({ msg: "Invalid token or user not found" });
     }
 
-    if(user) {
-    // Verify the token
-    jwt.verify(user.Token, process.env.JWT_SECRET, async (err, decoded) => {
-      if (err) {
-        await registerCollections.updateOne(
-          { UserID: user.UserID },
-          { $set: { Token: null } }
-        );
-        return res.status(401).json({
-          msg: "Token expired. Please try to reset with email again.",
-          error: err.message,
-        });
-      }else {
-      // Hash the new password and update the user's password
-      bcrypt.hash(newPassword, 10, async (err, hash) => {
+    if (user) {
+      // Verify the token
+      jwt.verify(user.Token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
-          return res.status(500).json({ msg: "Something went wrong", error: err.message });
+          await registerCollections.updateOne(
+            { UserID: user.UserID },
+            { $set: { Token: null } }
+          );
+          return res.status(401).send({
+            msg: "Token expired. Please try to reset with email again.",
+            error: err.message,
+          });
+        } else {
+          // Hash the new password and update the user's password
+          bcrypt.hash(newPassword, 10, async (err, hash) => {
+            if (err) {
+              return res
+                .status(500)
+                .send({ msg: "Something went wrong", error: err.message });
+            }
+
+            await registerCollections.updateOne(
+              { UserID: user.UserID },
+              { $set: { password: hash, confirmPassword: hash } }
+            );
+
+            return res.status(200).send({ msg: "Password reset successful" });
+          });
         }
-
-        await registerCollections.updateOne(
-          { UserID: user.UserID },
-          { $set: { password: hash, confirmPassword: hash } }
-        );
-
-        return res.status(200).json({ msg: "Password reset successful" });
       });
     }
-    });
-  }
   } catch (error) {
     return res.status(500).json({ msg: "Server error", error: error.message });
   }
